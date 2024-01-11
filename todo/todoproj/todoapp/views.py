8ad87082bc004ceb961f43624904f4b8a42cb0b2
malavika <malavikarajeevan85@gmail.com>
2024-01-11 09:22:00 +0530

    new file added

--- todo/todoproj/todoapp/views.py
+++ todo/todoproj/todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import forms
 
 # Create your views here.
 def Home(req):
@@ -18,15 +19,22 @@ def Home(req):
     return render(req,"index.html",{"tasks":tasks})
 
 def update(req,id):
-    print(id)
-    task=Task.objects.get(id=id)
-    if req.method=="POST":
-        task=req.POST.get('task','')
-        priority=req.POST.get('priority','')
-        Task.objects.filter(id=id).update(task=task,priority=priority)
+    # print(id)
+    # task=Task.objects.get(id=id)
+    # if req.method=="POST":
+    #     task=req.POST.get('task','')
+    #     priority=req.POST.get('priority','')
+    #     Task.objects.filter(id=id).update(task=task,priority=priority)
+    #     return redirect("home")
+    #=========================================
+    #django forms
+    f=TodoForm(req,POST or None,instance=tasks)
+    if f.is_valid():
+        f.save()
         return redirect("home")
     
-    return render(req,'update.html',{"task":task})
+    # return render(req,'update.html',{"task":task})
+    return render(req, 'formupdate.html',{"task":tasks,'f':f})
 
 def delete(req,id):
     task=Task.objects.get(id=id)
